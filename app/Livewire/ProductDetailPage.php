b03ab31a41746048ba5122e28ff006b2c32c36d5
kamarul-ariffin-github <mkamarulariffinrosli@gmail.com>
2024-03-18 00:36:46 +0800

    updates

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,7 +2,10 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Product;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -10,10 +13,36 @@ use Livewire\Component;
 class ProductDetailPage extends Component
 {
 
+    use LivewireAlert;
+
     public $slug;
+    public $quantity=1;
 
     public function mount($slug){
         $this->slug = $slug;
+    }
+
+    public function increaseQty(){
+        $this->quantity++;
+    }
+
+    public function decreaseQty(){
+        if($this->quantity > 1){
+            $this->quantity--;
+        }
+    }
+
+    public function addToCart($product_id){
+        $total_count = CartManagement::addItemToCartWithQty($product_id, $this->quantity);
+
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+
+        $this->alert('success', 'Product added to the cart successfully!', [
+            'position' => 'bottom-end',
+            'timer' => 3000,
+            'toast' => true,
+            'timerProgressBar' => true,
+           ]);
     }
 
     public function render()
